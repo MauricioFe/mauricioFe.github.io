f86194672a9cb65a9627adc84fa63bddaaf65104
MauricioFe <mauricio.lacerdaml@gmail.com>
2020-07-05 01:52:30 -0300

    Menu mobile

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,13 @@
+var hamburger = document.querySelector("#burger");
+var nav = document.querySelector("#menu-superior");
 
 
 
 
+hamburger.addEventListener("click", () => {
+    if (!nav.classList.contains("ativo"))
+        nav.classList.add("ativo");
+    else
+        nav.classList.remove("ativo");
+})
 
