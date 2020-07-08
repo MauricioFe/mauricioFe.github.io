eeff0a54f7fe75c1f78aa73c9e83cd4e0af39753
MauricioFe <mauricio.lacerdaml@gmail.com>
2020-07-08 18:16:25 -0300

    Update carrossel

--- js/carrossel.js
+++ js/carrossel.js
@@ -1,56 +1,53 @@
-var proximaFrase = 1; //Guarda a posição de uma frase. Ele inicia com 1 já que a posição 0 e da frase que aparece primeiro
-var zIndexAtual = 0; //É uma variável para fazer o incremento do z-index da frase atual
-var frases = document.querySelector('#slider').children; //vetor que armazena todas as frases
-function currentSlide(n) {
-    proximaFrase = n;
-    avancarFrase();
+var slideIndex = 1;
+
+var myTimer;
+
+var slideshowContainer;
+
+window.addEventListener("load",function() {
+    showSlides(slideIndex);
+    myTimer = setInterval(function(){plusSlides(1)}, 4000);
+})
+
+// NEXT AND PREVIOUS CONTROL
+function plusSlides(currentSlide){
+  clearInterval(myTimer);
+  if (currentSlide < 0){
+    showSlides(slideIndex -= 1);
+  } else {
+   showSlides(slideIndex += 1); 
+  }
+  
+ 
+  if (currentSlide === -1){
+    myTimer = setInterval(function(){plusSlides(currentSlide + 2)}, 4000);
+  } else {
+    myTimer = setInterval(function(){plusSlides(currentSlide + 1)}, 4000);
+  }
 }
-function avancarFrase() {
-console.log(proximaFrase)
-    frase = frases[proximaFrase]; //variavel que pegar a proxima frase a ser mostrada;
-    frase.style.zIndex = ++zIndexAtual;// increnta o zIndex da frase atual;
-    frase.style.marginLeft = "0%" //reseta a margin da frase fazendo ela aparecer 
 
-    proximaFrase++; //Alterna de imagem
-
-    if (proximaFrase >= frases.length) {
-        proximaFrase = 0; //reinicia a variavel
-    }
-    //Essa função permite que as frases que já passaram voltem para fora da área visível
-    setTimeout(resetarFrase, 1500);
+//Controls the current slide and resets interval if needed
+function currentSlide(currentSlide){
+  clearInterval(myTimer);
+  myTimer = setInterval(function(){plusSlides(currentSlide + 1)}, 4000);
+  showSlides(slideIndex = currentSlide);
 }
-function resetarFrase() {
-    fraseVisivel = proximaFrase - 1;
-    // se a frase visivel alcançou o inicio da fila, voltar para o final
-    if (fraseVisivel < 0)
-        fraseVisivel = frases.length - 1;
 
-    for (var i = 0; i < frases.length; i++) {
-        if (i != fraseVisivel)
-            frases[i].style.marginLeft = "100%"
-    }
+function showSlides(currentSlide){
+  var i;
+  var slides = document.getElementsByClassName("frase");
+  var dots = document.getElementsByClassName("dot");
+  if (currentSlide > slides.length) {slideIndex = 1}
+  if (currentSlide < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].classList.remove("active");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].classList.add("active");
 }
-setInterval(avancarFrase, 5000);//chamada do método
 
-// var slideIndex = 1;
-// function currentSlide(n) {
-//     showSlides(slideIndex = n);
-// }
 
-// function showSlides(n) {
-//     var i;
-//     var slides = document.querySelectorAll(".frase");
-//     var dots = document.querySelectorAll(".dot");
-//     if (n > slides.length) { slideIndex = 1 }
-//     if (n < 1) { slideIndex = slides.length }
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none";
-//     }
-//     for (i = 0; i < dots.length; i++) {
-//         dots[i].classList.remove("active");
-//     }
-//     slides[slideIndex - 1].style.display = "block";
-//     dots[slideIndex - 1].classList.add("active");
-// }
 
-//setInterval(function () { showSlides(slideIndex) }, 3000)
